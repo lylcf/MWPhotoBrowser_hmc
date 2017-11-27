0682cba8b5f6ad0168cce08684a121d1c825a56b
Moin Uddin <moinku07@gmail.com>
2017-11-27 13:57:59 +0600

    MWPhoto.m updated for SDWebImage 4.2.2

--- Pod/Classes/MWPhoto.m
+++ Pod/Classes/MWPhoto.m
@@ -209,34 +209,24 @@
 
 // Load from local file
 - (void)_performLoadUnderlyingImageAndNotifyWithWebURL:(NSURL *)url {
-    @try {
-        SDWebImageManager *manager = [SDWebImageManager sharedManager];
-        _webImageOperation = [manager downloadImageWithURL:url
-                                                   options:0
-                                                  progress:^(NSInteger receivedSize, NSInteger expectedSize) {
-                                                      if (expectedSize > 0) {
-                                                          float progress = receivedSize / (float)expectedSize;
-                                                          NSDictionary* dict = [NSDictionary dictionaryWithObjectsAndKeys:
-                                                                                [NSNumber numberWithFloat:progress], @"progress",
-                                                                                self, @"photo", nil];
-                                                          [[NSNotificationCenter defaultCenter] postNotificationName:MWPHOTO_PROGRESS_NOTIFICATION object:dict];
-                                                      }
-                                                  }
-                                                 completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL) {
-                                                     if (error) {
-                                                         MWLog(@"SDWebImage failed to download image: %@", error);
-                                                     }
-                                                     _webImageOperation = nil;
-                                                     self.underlyingImage = image;
-                                                     dispatch_async(dispatch_get_main_queue(), ^{
-                                                         [self imageLoadingComplete];
-                                                     });
-                                                 }];
-    } @catch (NSException *e) {
-        MWLog(@"Photo from web: %@", e);
+    [[[SDWebImageManager sharedManager] imageDownloader] downloadImageWithURL:url options:0 progress:^(NSInteger receivedSize, NSInteger expectedSize, NSURL * _Nullable targetURL) {
+        if (expectedSize > 0) {
+            float progress = receivedSize / (float)expectedSize;
+            NSDictionary* dict = [NSDictionary dictionaryWithObjectsAndKeys:
+                                  [NSNumber numberWithFloat:progress], @"progress",
+                                  self, @"photo", nil];
+            [[NSNotificationCenter defaultCenter] postNotificationName:MWPHOTO_PROGRESS_NOTIFICATION object:dict];
+        }
+    } completed:^(UIImage * _Nullable image, NSData * _Nullable data, NSError * _Nullable error, BOOL finished) {
+        if (error) {
+            MWLog(@"SDWebImage failed to download image: %@", error);
+        }
         _webImageOperation = nil;
-        [self imageLoadingComplete];
-    }
+        self.underlyingImage = image;
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self imageLoadingComplete];
+        });
+    }];
 }
 
 // Load from local file
